5e2467c8b586975c5a07ec12a6246f9835cf0cd9
WespertoW <ghreinocraft@gmail.com>
2026-04-15 18:17:31 -0300

    add: exercicio 8 concluido

---
+++ Exercicio8.py
@@ -0,0 +1,11 @@
+#list comprehension
+# valores_funcionaro = [float(x) for x in input().split()]
+
+# print(f"NUMBER = {int(valores_funcionaro[0])}\nSALARY = U$ {valores_funcionaro[1]*valores_funcionaro[2]:.2f}")
+
+#metodo padrõa
+codigo_funcionario = str(input())
+hora_trabalhada_funcionario = float(input())
+salario_funcionario = float(input())
+
+print(f"NUMBER = {codigo_funcionario}\nSALARY = U$ {hora_trabalhada_funcionario*salario_funcionario:.2f}")
